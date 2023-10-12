fdaa15897428ed3c0ac8e84fc638cbd10bbca940
Iryna220 <ibiriszka951@gmail.com>
2023-10-12 13:24:59 +0200

    update hw8

--- src/components/PrivatRoute/PrivatRoute.jsx
+++ src/components/PrivatRoute/PrivatRoute.jsx
@@ -1,14 +1,14 @@
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 import { getAuth } from 'redux/auth/authSelectors';
-import Loader from 'components/Loader/Loader';
+// import Loader from 'components/Loader/Loader';
 
 const PrivatRoute = () => {
   const { isLogin, token } = useSelector(getAuth);
 
-  if (!isLogin && !token) {
-    return <Loader />;
-  }
+  // if (!isLogin && !token) {
+  //   return <Loader />;
+  // }
 
   if (!isLogin && !token) {
     return <Navigate to="/login" />;
